3fc0355359e34fd7f45b4f1764a9e440d6c33a03
mvpc-11232 <mvp2612@hotmail.com>
2022-09-08 18:27:58 +0530

    Auto Push Commit

--- terraform-mutable/main.tf
+++ terraform-mutable/main.tf
@@ -2,16 +2,12 @@
 #   source = "git::https://github.com/b49-clouddevops/tf-module-vpc.git?ref=main"
 # }
 
-module "mongodb" {
-  source           = "./vendor/modules/mongodb"
-  ENV              = var.ENV
-  WORKSPATION_IP   = var.WORKSPATION_IP
-  DOCDB_PORT       = var.DOCDB_PORT 
-  DOCDB_INSTACE_CLASS = var.DOCDB_INSTACE_CLASS
-  DOCDB_INSTACE_COUNT = var.DOCDB_INSTACE_COUNT
+module "cart" {
+  source           = "./vendor/modules/app"
+  COMPONENT        = var.COMPONENT
 }
 
-}
+
 
 # You cannot parameterise the source section of the terraform module : Branc name cannot be parameterised. 
 # That's the challenge with terraform : Terrafile from coretech
