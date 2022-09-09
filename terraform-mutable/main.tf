20d1e6039566f9d940ca0d6233acf6a4c61d3f7e
mvpc-11232 <mvp2612@hotmail.com>
2022-09-09 08:15:19 +0530

    Auto Push Commit

--- terraform-mutable/main.tf
+++ terraform-mutable/main.tf
@@ -8,6 +8,14 @@ module "cart" {
 }
 
 
+variable "INSTANCE_TYPE" {}
+variable "ENV" {}
+variable "SPOT_INSTANCE_COUNT" {}
+variable "OD_INSTANCE_COUNT" {}
+variable "COMPONENT" {}
+variable "APP_PORT" {}
+variable "WORKSPATION_IP" {}
+
 
 # You cannot parameterise the source section of the terraform module : Branc name cannot be parameterised. 
 # That's the challenge with terraform : Terrafile from coretech
